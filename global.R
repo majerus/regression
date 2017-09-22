d254707b15102b41c06124a1bfcc0060d0d61222
majerus <rich.majerus@gmail.com>
2017-09-22 09:19:10 -0400

    Update global.R

--- global.R
+++ global.R
@@ -1,9 +1,7 @@
 
-library(shinyapps)
 library(googleVis)
 library(knitr)
 library(shiny)
-library(shinysky)
 
 download.file("http://www.openintro.org/stat/data/mlb11.RData", destfile = "mlb11.RData")
 load("mlb11.RData")
